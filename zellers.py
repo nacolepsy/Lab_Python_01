479091b2777cc96319eae86396608f8665a701e7
Nacolepsy <kwamas2001@yahoo.com>
2012-06-20 11:25:03 +0000

    Updated assignment

---
+++ zellers.py
@@ -0,0 +1,6 @@
+first_name = raw_input("Please Enter Your First Name: ")
+last_name=raw_input ("Please Enter your Last Name:" )
+
+print ("These are the months of the year \n March=1\n April=2\n May=3\n June=4\n July=5 \n August=6\n September=7\n October=8 \n November=9\n december=10 \n January=11\n February=12")
+dob= raw_input ("Please Enter Your Date of Birth:" )
+print(first_name+' '+last_name+' '+"was born on"+' '+dob)
