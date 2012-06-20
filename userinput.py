9a004632d2d4ffd8633d0e8aea74e5a56100390c
Nacolepsy <kwamas2001@yahoo.com>
2012-06-20 11:05:06 +0000

    This is for Day 2

---
+++ userinput.py
@@ -0,0 +1,4 @@
+first_name = raw_input("Please Enter Your First Name: ")
+last_name=raw_input ("Please Enter your Last Name:" )
+dob= raw_input ("Please Enter Your Date of Birth:" )
+print(first_name+' '+last_name+' '+"was born on"+' '+dob)
